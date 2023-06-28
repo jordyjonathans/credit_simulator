4c354c6c437509867c17ba9d3bf95f7e69543bd6
aditya <aditya.djauhari@mncgroup.com>
2023-06-28 17:14:53 +0700

    test push 1

--- src/main/java/org/example/Controllers/CreditController.java
+++ src/main/java/org/example/Controllers/CreditController.java
@@ -30,6 +30,7 @@ public class CreditController {
         installment = new ArrayList<HashMap<String,String>>();
         view.run();
     }
+
     public CreditController(String param) {
         view = new CreditView();
         this.view.setController(this);
